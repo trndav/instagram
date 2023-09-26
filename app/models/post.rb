00b233ba9df243adcf58bb7f5001faeb3c3c9e00
trndav <farsiteguide@gmail.com>
2023-09-26 13:50:19 +0200

    Added post validations and post error message stylings

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+    validates :title, presence: true, length: { minimum: 5, maximum: 100 }
+    validates :description, :keywords, presence: true, length: { minimum: 5, maximum: 400 }
 end
