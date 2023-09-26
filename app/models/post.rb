99212ac3d408ef0fab933b4c0e12a63cecdea970
trndav <farsiteguide@gmail.com>
2023-09-26 15:44:29 +0200

    Added images upload with post functionality Active-storage, has_many association, image show styling

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
-    validates :title, presence: true, length: { minimum: 5, maximum: 100 }
-    validates :description, :keywords, presence: true, length: { minimum: 5, maximum: 400 }
+    validates :title, presence: true, length: { minimum: 3, maximum: 100 }
+    validates :description, :keywords, presence: true, length: { minimum: 4, maximum: 400 }
+
+    has_many_attached :images
 end
