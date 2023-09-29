5544be05c2ed20e24e5a1cd65749571a6990616e
trndav <farsiteguide@gmail.com>
2023-09-29 14:36:01 +0200

    Add Bio and Username field to users Sign up and Edit page - users can edit profile without typing password

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,7 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
  before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+ before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   # def new
@@ -42,13 +42,22 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
  def configure_sign_up_params
-   devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :bio])
+   devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :bio, :avatar])
  end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+def configure_account_update_params
+  devise_parameter_sanitizer.permit(:account_update, keys: [:username, :bio, :avatar])
+end
+
+# My script
+def update_resource(resource, params)
+  # Require password if user is trying to change password
+  return super if params["password"]&.present?
+
+  # Allows user to update registration info without password
+  resource.update_without_password(params.except("current_password"))
+end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
